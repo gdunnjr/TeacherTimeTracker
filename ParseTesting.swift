79e8d788c2d35351a55ef2188c636c7500bafe96
Gerald Dunn <gdunnjr@yahoo.com>
2015-08-07 05:33:08 -0400

    Finished playing with Parse. Moved testing code out and cleaned up

---
+++ ParseTesting.swift
@@ -0,0 +1,188 @@
+//
+//  ParseTesting.swift
+//  TeacherTimeTracker
+//
+//  Created by Gerald Dunn on 8/7/15.
+//  Copyright (c) 2015 Gerald Dunn. All rights reserved.
+//
+
+import Foundation
+
+
+func createParseTestTimeEntryObject(testObjectName: String) {
+    var testObject = PFObject(className:"TestTimeEntry")
+    testObject["timeEntryName"] = testObjectName
+    testObject.saveInBackground()
+}
+
+func createParseTestSubjectObject(subjectName: String) {
+    var subObj1 = PFObject(className:"TestSubject")
+    subObj1["testSubjectName"] = subjectName
+    subObj1.saveInBackground()
+}
+
+
+// example of getting an obect asynchronously
+func getTestSubjectObjectAsync(subjectName: String) {
+    var testQuery = PFQuery(className: "TestSubject")
+    var mySubject = PFObject(className: "TestSubject")
+    testQuery.whereKey("testSubjectName", equalTo: subjectName)
+    testQuery.findObjectsInBackgroundWithBlock {
+        (objects: [AnyObject]!, error: NSError!) -> Void in
+        if error == nil {
+            for object in objects {
+                mySubject = object as PFObject
+                println( object)
+                println( mySubject)
+                
+                if let serviceTypeUnwrap = mySubject["testSubjectName"] as? String {
+                    let mySubjectName = mySubject["testSubjectName"] as String
+                    println(serviceTypeUnwrap)
+                }
+            }
+            
+        } else {
+            
+            // Log details of the failure
+            NSLog("Error: %@ %@", error, error.userInfo!)
+        }
+    }
+}
+
+// working example of how to a synchronous query on main thread
+// this is not reccomended - timeouts will lock up the app/phone
+func getTestSubjectObjectSynchrnous(subjectName: String) -> PFObject {
+    var testQuery = PFQuery(className: "TestSubject")
+    var myPFObject = PFObject(className: "TestSubject")
+    testQuery.whereKey("testSubjectName", equalTo: subjectName)
+    var messages = testQuery.findObjects() as [PFObject]
+    for message in messages { // message is of PFObject type
+        println(message)
+        myPFObject = message
+    }
+    return myPFObject
+}
+
+
+func getTestSubject(
+    subjectNameFilter:String,
+    completion: (result: PFObject)->Void)
+{
+    var query = PFQuery(className:"TestSubject")
+    var myPFObject = PFObject(className:"TestSubject")
+    
+    //if sortDescriptor.key != "" {
+    //    query.orderBySortDescriptor(sortDescriptor)
+    //}
+    query.whereKey("testSubjectName", equalTo: subjectNameFilter)
+    
+    query.findObjectsInBackgroundWithBlock {
+        (objects: [AnyObject]!, error: NSError!) -> Void in
+        if error == nil {
+            
+            // Do something with the found objects
+            for object in objects {
+                // self.arrayOfObjects.append(object[parseObject]!!)
+                myPFObject = object as PFObject
+            }
+            
+        } else {
+            // Log details of the failure
+            println("Error: \(error) \(error.userInfo!)")
+        }
+        completion(result:myPFObject)
+    }
+    
+}
+
+// Example of how to iterate over relationships
+func GetRelationships() {
+    var testQuery = PFQuery(className: "TestObject")
+    testQuery.whereKey("foo", equalTo: "Test Me New 92")
+    testQuery.findObjectsInBackgroundWithBlock {
+        (objects: [AnyObject]!, error: NSError!) -> Void in
+        if error == nil {
+            for object in objects {
+                println(object)
+                var relation = object.relationForKey("subjectPtr")
+                
+                // code to iterate relationships
+                relation.query().findObjectsInBackgroundWithBlock {
+                    (relationObjects: [AnyObject]!, error: NSError?) -> Void in
+                    if let error = error {
+                        // There was an error
+                    } else {
+                        // objects has all the Posts the current user liked.
+                        
+                        for relationObject in relationObjects {
+                            //println(relationObject)
+                            var subjectName = ""
+                            if let subjectNameUnwrap = relationObject["subjectName"] as? String {
+                                subjectName = relationObject["subjectName"] as String
+                                println(subjectName)
+                            }
+                        }
+                        
+                    }
+                }
+                
+            }
+            
+        } else {
+            
+            // Log details of the failure
+            NSLog("Error: %@ %@", error, error.userInfo!)
+        }
+    }
+}
+
+
+// Code to add relationships - due to asynchrounous nature of the save and
+//  query calls, this can get tricky
+func AddRelationship() {
+    var subObj1 = PFObject(className:"subject")
+    subObj1["subjectName"] = "Prog 900"
+    subObj1.saveInBackground()
+
+    var subObj2 = PFObject(className:"subject")
+    subObj2["subjectName"] = "Prog 901"
+    subObj2.saveInBackground()
+
+
+    var testObject = PFObject(className:"TestObject")
+    testObject["foo"] = "Test Me New 3"
+    testObject.saveInBackground()
+
+    var testQuery = PFQuery(className: "TestObject")
+    testQuery.whereKey("foo", equalTo: "Test Me New 3")
+    testQuery.findObjectsInBackgroundWithBlock {
+        (objects: [AnyObject]!, error: NSError!) -> Void in
+        if error == nil {
+            for object in objects {
+                println(object)
+                
+                // code to add relations
+                let myPFObject = object as PFObject
+                var relation = object.relationForKey("subjectPtr")
+                relation.addObject(subObj1)
+                relation.addObject(subObj2)
+                myPFObject.saveInBackgroundWithBlock {
+                    (success: Bool!, error: NSError!) -> Void in
+                    if (success != nil) {
+                        println("Saved")
+                    } else {
+                        println("%@", error)
+                    }
+                }
+            }
+            
+        } else {
+            
+            // Log details of the failure
+            NSLog("Error: %@ %@", error, error.userInfo!)
+        }
+    }
+}
+
+
+ 
--- TabBarViewController.swift
+++ TabBarViewController.swift
@@ -13,190 +13,6 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        var testQuery = PFQuery(className: "TestObject")
-        testQuery.whereKey("foo", equalTo: "Test Me New 92")
-        testQuery.findObjectsInBackgroundWithBlock {
-            (objects: [AnyObject]!, error: NSError!) -> Void in
-            if error == nil {
-                for object in objects {
-                    println(object)
-                    var relation = object.relationForKey("subjectPtr")
-                    
-                    // code to iterate relationships
-                    relation.query().findObjectsInBackgroundWithBlock {
-                        (relationObjects: [AnyObject]!, error: NSError?) -> Void in
-                        if let error = error {
-                            // There was an error
-                        } else {
-                            // objects has all the Posts the current user liked.
-                       
-                            for relationObject in relationObjects {
-                                //println(relationObject)
-                                var subjectName = ""
-                                if let subjectNameUnwrap = relationObject["subjectName"] as? String {
-                                    subjectName = relationObject["subjectName"] as String
-                                    println(subjectName)
-                                }
-                            }
-                            
-                        }
-                    }
-                    
-                }
-
-            } else {
-            
-                // Log details of the failure
-                NSLog("Error: %@ %@", error, error.userInfo!)
-            }
-    }
-    
-
-    
-    
-    
-/*
-        var subObj1 = PFObject(className:"subject")
-        subObj1["subjectName"] = "Prog 900"
-        subObj1.saveInBackground()
-        
-        var subObj2 = PFObject(className:"subject")
-        subObj2["subjectName"] = "Prog 901"
-        subObj2.saveInBackground()
-        
-        
-        var testObject = PFObject(className:"TestObject")
-        testObject["foo"] = "Test Me New 3"
-        testObject.saveInBackground()
-        
-        var testQuery = PFQuery(className: "TestObject")
-        testQuery.whereKey("foo", equalTo: "Test Me New 3")
-        testQuery.findObjectsInBackgroundWithBlock {
-            (objects: [AnyObject]!, error: NSError!) -> Void in
-            
-            if error == nil {
-                for object in objects {
-                    println(object)
-                
-                // code to add relations
-                let myPFObject = object as PFObject
-                var relation = object.relationForKey("subjectPtr")
-                relation.addObject(subObj1)
-                relation.addObject(subObj2)
-                myPFObject.saveInBackgroundWithBlock {
-                        (success: Bool!, error: NSError!) -> Void in
-                        if (success != nil) {
-                            println("Saved")
-                        } else {
-                            println("%@", error)
-                        }
-                    }
-                    
-                /* myPFObject.setObject("Testing",forKey: "foo")
-                 
-                //myPFObject.setObject(subObj1,forKey: "subjectPtr1")
-                myPFObject.setObject([subObj1, subObj2] ,forKey: "subjectPtr1")
-                    myPFObject.saveInBackgroundWithBlock {
-                        (success: Bool!, error: NSError!) -> Void in
-                        if (success != nil) {
-                            println("Saved")
-                        } else {
-                            println("%@", error)
-                        }
-                    }
-                    
-                    var subjectName = ""
-                    if let pointer = object["subjectPtr"] as? PFObject {
-                        subjectName = pointer["subjectName"] as String!
-                        println(pointer)
-                    }
-
-*/
-                }
-
-                
-            } else {
-            
-                // Log details of the failure
-                NSLog("Error: %@ %@", error, error.userInfo!)
-            }
-        }
-        
-*/
-
-     /*
-        
-        var query = PFQuery(className: "timeentry")
-        query.includeKey("subjectPtr")
-        query.findObjectsInBackgroundWithBlock {
-            (objects: [AnyObject]!, error: NSError!) -> Void in
-            
-            if error == nil {
-                
-                // query successful - display number of rows found
-                println("Successfully retrieved \(objects.count) time entries")
-                
-                // print name & hair color of each person found
-                for object in objects {
-                    
-                    //let startDateTime = object["startDateTime"] as NSString
-                    //var startDateTime = object["startDateTime"]
-                    //if startDateTime != nil {
-                    //    let dateFormatter = NSDateFormatter()
-                    //    dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.SSS'Z'"
-                    //    let date = dateFormatter.stringFromDate(startDateTime as NSDate)
-                    //    println(date)
-                    //}
-                    //var serviceType = ""
-                    //if let serviceTypeUnwrap = object["serviceType"] as? String {
-                    //    serviceType = object["serviceType"] as String
-                   // }
-                    
-                    //var subjectName = ""
-                    //if let subjectNameUnwrap = object["subjectName"] as? String {
-                    //    subjectName = object["subjectName"] as String
-                    //}
-                    
-                    
-                    let notes = object["notes"] as NSString
-        
-                    //var subjectPtrObject =  object["subjectPtr"] //Since it automatically loaded the data inside the subclass, you don't actually have to set any variable
-                    //var subjectName = ""
-                    //if let subjectNameUnwrap = subjectPtrObject as? String {
-                    //    subjectName = subjectPtrObject as String
-                    //}
-                    
-                    var subjectName = ""
-                    if let pointer = object["subjectPtr"] as? PFObject {
-                        subjectName = pointer["subjectName"] as String!
-                    }
-                    
-                    //let subjectName = object["subjectName"] as NSString
-                    
-                    
-                    //println(subjectPtrObject)
-                    println(subjectName)
-                    println(notes)
-                    
-                    
-  //                  println(object)
-//
-    //                println("subjectName = \(subjectName)")
-      //              println("notes = \(notes)")
-        //            println("serviceType = " + serviceType)
-                    
-                    
-                }
-            } else {
-                
-                // Log details of the failure
-                NSLog("Error: %@ %@", error, error.userInfo!)
-            }
-        }
-       */
-        
-
         // Do any additional setup after loading the view.
     }
 
@@ -215,5 +31,32 @@ class TabBarViewController: UITabBarController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    
+    // MARK: These functions were written to kick the tires on parse. 
+    // No purpose for them now so commented out
+    /*
+    func TestOutParse() {
+        //createParseTestSubjectObject("Math")
+        createParseTestSubjectObject("Science")
+        
+        // asynchrounous example
+        getTestSubjectObjectAsync("Math")
+        
+        
+        var myPFMathObject = PFObject(className:"TestSubject")
+        myPFMathObject = getTestSubjectObjectSynchrnous("Math")
+        println(myPFMathObject)
+        
+        // Example of using a completion block - still async
+        getTestSubject("Math")
+            {
+                (result: PFObject) in
+                //println(result)
+                var myPFObject = result as PFObject
+                //println(myPFObject)
+                
+        }
+    }
+    */
 }
